78b4b57cf7e325a0ca0fb67548786754afadb671
Kaede.Kodama <kaede.kodama@rakuten.com>
2018-06-07 07:19:02 +0000

    user pages

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,8 +13,8 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
 		@if (Auth::check())
-                        <li><a href="#">Users</a></li>
-                        <li class="dropdown">
+			<li>{!! link_to_route('users.index', 'Users') !!}</li>
+			                <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">My profile</a></li>
@@ -24,7 +24,7 @@
                         </li>
                  @else
 		       <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
-                       <li><a href="#">Login</a></li>
+               <li>{!! link_to_route('login', 'Login') !!}</li>
                 @endif            
 
     		</ul>
